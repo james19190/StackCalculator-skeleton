8a9e2a9f635734bb354bcd336091e09b4cfcddb3
James Kyoung Ho Kim <james19190@gmail.com>
2024-04-01 22:40:36 +0900

    Add MyQueue class code

--- src/com/dmlab/util/MyQueue.java
+++ src/com/dmlab/util/MyQueue.java
@@ -28,25 +28,42 @@ public class MyQueue<E> implements Queue<E>{
 	@Override
 	public void add(E item) throws RuntimeException {
 		/* Code Here */
+		if (count == 128) {
+			throw new RuntimeException("exceed the limitation of the internal array space");
+		}
+		else {
+			mData[mCursor++] = item;
+			count++;
+		}
 
 	}
 
 	@Override
 	public E poll() throws EmptyQueueException {
 		/* Code Here */
-
+		if(empty()){
+			throw new EmptyQueueException();
+		}
+		E item = mData[mHead];
+		mHead = (mHead + 1) % 128;
+		count--;
+		return item;
 	}
 
 	@Override
 	public E peek() throws EmptyQueueException {
 		/* Code Here */
+		if (count == 0) {
+			throw new EmptyQueueException();
+		}
+		return mData[mHead];
 
 	}
 
 	@Override
 	public boolean empty() {
 		/* Code Here */
-
+		return count == 0;
 	}
 
 }
